aefcd18b48baf933db52ef76c9de651414695986
yangjie6020 <yangjie6020@gmail.com>
2015-11-05 19:59:45 +0800

    修复标题太长的排列问题

--- v2exMe/app/src/main/java/com/bigfish/v2exme/ui/NodeTopicsActivity.java
+++ v2exMe/app/src/main/java/com/bigfish/v2exme/ui/NodeTopicsActivity.java
@@ -3,8 +3,6 @@ package com.bigfish.v2exme.ui;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
